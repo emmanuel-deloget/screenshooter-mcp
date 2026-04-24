a10f015c5b2a321451b13d59d59012ae8d3d6738
Emmanuel Deloget <emmanuel@deloget.com>
2026-04-24 10:41:44 +0200

    tests: reject Fedora 43 X11 combinations (not supported)
    Fedora 43 does not support X11. Reject both fedora-43-gnome-x11 and
    fedora-43-kde-x11 combinations at startup to provide clear feedback.
    
    Signed-off-by: Emmanuel Deloget <emmanuel@deloget.com>

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -83,6 +83,13 @@ case "$DESKTOP" in
 		;;
 esac
 
+case "${DISTRO}-${VERSION}-${DESKTOP}-${MODE}" in
+	fedora-43-gnome-x11|fedora-43-kde-x11)
+		echo "Unsupported combination: ${DISTRO}, ${VERSION}, ${DESKTOP}, ${MODE}, ignoring"
+		exit 0
+		;;
+esac
+
 VM_NAME="test-${DISTRO}-${VERSION}-${DESKTOP}-${MODE}"
 BASE_IMAGE="${BASES_DIR}/${DISTRO}-${VERSION}-${DESKTOP}.qcow2"
 VM_IMAGE="${VMS_DIR}/${VM_NAME}.qcow2"
